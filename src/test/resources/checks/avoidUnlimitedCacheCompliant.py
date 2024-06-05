316958a90097161d60d2f773a262b32575a6bfdd
B3ND3L <benjamin.delahais@proton.me>
2024-06-05 09:00:08 +0200

    [Issue #26][EC89] Changer version ecocode-rules to 1.6.0

--- src/test/resources/checks/avoidUnlimitedCacheCompliant.py
+++ src/test/resources/checks/avoidUnlimitedCacheCompliant.py
@@ -1,13 +1,16 @@
-import functools
+from functools import lru_cache
+
 
 @lru_cache
 def cached_function():
     print('a')
 
+
 @lru_cache()
-def cached_function():
+def cached_function_a():
     print('a')
 
+
 @lru_cache(maxsize=30)
-def cached_function():
+def cached_function_b():
     print('b')
--- src/test/resources/checks/avoidUnlimitedCacheNonCompliant.py
+++ src/test/resources/checks/avoidUnlimitedCacheNonCompliant.py
@@ -1,17 +1,21 @@
-import functools
+from functools import cache
+from functools import lru_cache
+
 
 class A:
     @cache  # Noncompliant {{Do not set cache size to unlimited}}
-    def cached_method_a():
+    def cached_method_a(self):
         print('a')
 
     @lru_cache(maxsize=None)  # Noncompliant {{Do not set cache size to unlimited}}
-    def cached_method_b():
+    def cached_method_b(self):
         print('b')
+
 
 @cache  # Noncompliant {{Do not set cache size to unlimited}}
 def cached_function():
     print('a')
+
 
 @lru_cache(maxsize=None)  # Noncompliant {{Do not set cache size to unlimited}}
 def cached_method():
